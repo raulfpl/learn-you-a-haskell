0c1120b211f29650ccb2a3873350508881c90010
Raul Felipe <raulfpl@gmail.com>
2014-02-18 03:13:45 -0300

    Finish chapter 2

---
+++ chapter2/ch2-starting-out.hs
@@ -0,0 +1,11 @@
+-- Baby's first functions
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+-- I'm a list comprehension
+boomBang xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+length' xs = sum [ 1 | _ <- xs ]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
